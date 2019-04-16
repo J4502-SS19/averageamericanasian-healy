7e68d0cb4def14889f96b0ceddd900fa14aa2ef1
averageamericanasian <38271812+averageamericanasian@users.noreply.github.com>
2019-04-16 10:25:54 -0500

    april 16

--- JavaScript examples w12/JavaScript examples w12/js_marquis/js/script.js
+++ JavaScript examples w12/JavaScript examples w12/js_marquis/js/script.js
@@ -4,9 +4,26 @@ var allParagraphs=document.getElementsByTagName("p");
 
 var firstParagraph=allParagraphs[0];
 
+function revealCopy(e) {
+  // console.log( e );
+  var allParagraphs=document.getElementsByTagName("p");
+  for( var i=0; i < allParagraphs.length; i++ ){
+    // console.log(allParagraphs[i]);
+      if(i===0) {
+        continue;
+      }
+  allParagraphs[i].style.display="block";
+}
+  e.preventDefault();
+}
+
 newLink.setAttribute("href","#");
 
+newLin.setAttribute("class","more-link");
+
 newLink.innerHTML="Read More";
+
+newLink.addEventListener("click",revealCopy);
 
 firstParagraph.appendChild( newLink );
 
@@ -17,6 +34,9 @@ for( var i=0; i < allParagraphs.length; i++ ){
     }
   allParagraphs[i].style.display="none";
 }
+
+
+
 
 // newLink.setAttribute("class","more-link");
 
